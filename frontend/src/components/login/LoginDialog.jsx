35c3900737ba4494511a83d41b6dc6d22b9dbe1a
chinmayi <phchinmayi93@gmail.com>
2025-07-29 16:10:29 +0530

    Add Login UI

--- frontend/src/components/login/LoginDialog.jsx
+++ frontend/src/components/login/LoginDialog.jsx
@@ -1,5 +1,9 @@
-import { Dialog, TextField , Box} from "@mui/material";
+import { Dialog, TextField , Box, Button, Typography,styled} from "@mui/material";
 
+const Component = styled(Box)`
+    
+
+`
 const LoginDialog = ({ open, setOpen}) => {
 
     const handleClose = () => {
@@ -7,12 +11,18 @@ const LoginDialog = ({ open, setOpen}) => {
     }
     return (
         <Dialog open={open} onClose={handleClose}>
-            <Box>
+            <Component>
                 <Box></Box>
                 <Box>
                     <TextField variant="standard" label="Enter Email/Mobile No"/>
+                    <TextField variant="standard" label="Enter Password"/>
+                     <TextField variant="standard" label="By continuing, you agree to flipkart's Terms of use and Privacy Policy"/>
+                     <Button>Login</Button>
+                     <Typography>OR</Typography>
+                     <Button>Request OTP</Button>
+                     <Typography>New to Flipkart?Create an account</Typography>
                 </Box>
-            </Box>
+            </Component>
         </Dialog>
     );
 };
